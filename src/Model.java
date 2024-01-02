1bf35581d7b69fa3ce566fb8ed71cd66748a2bf9
axis133 <148345418+axis133@users.noreply.github.com>
2024-01-02 20:04:40 +0300

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -1,6 +1,4 @@
 interface Mobile {
-    void call(String number);
-    void sendMessage(String message);
 }
 
 abstract class SiemensMobile implements Mobile {
@@ -9,28 +7,10 @@ abstract class SiemensMobile implements Mobile {
     public SiemensMobile(String model) {
         this.model = model;
     }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
 }
 
 class Model extends SiemensMobile {
     public Model(String model) {
         super(model);
-    }
-
-    @Override
-    public void call(String number) {
-        System.out.println("Calling " + number + " from " + getModel());
-    }
-
-    @Override
-    public void sendMessage(String message) {
-        System.out.println("Sending message: " + message);
     }
 }
